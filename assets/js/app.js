58eb60e85ee6cf46b81d9fde1c7ffc572fc338f3
unknown <nikoladimitrijevic1991@gmail.com>
2019-03-20 21:04:01 +0100

    proto two

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+// shadow layers
+var shadowLayer = document.getElementsByClassName('shadowLayer');
+// black layer
 var blackLayer = document.getElementsByClassName('blackLayer')[0];
 // vid divs
 var redVidDiv = document.getElementsByClassName('redVidDiv')[0];
@@ -16,13 +19,21 @@ exitButton.addEventListener('click',()=>{
   BeYouTyDiv.style.width = '17.41vw';
   redVidDiv.style.height = '36.795vw';
   BeYouTyDiv.style.height = '36.795vw';
+  redVid.style.width = '100%';
+  redVid.style.height = '100%'
+  beyoutyVid.style.width = '100%';
+  beyoutyVid.style.height = '100%';
+  redVidDiv.style.zIndex = '0';
+  BeYouTyDiv.style.zIndex = '0';
+  shadowLayer[0].style.display = 'block';
+  shadowLayer[1].style.display = 'block';
+  shadowLayer[2].style.display = 'block';
+
 
 
   blackLayer.style.opacity = '0';
   setTimeout(function(){
     blackLayer.style.display = 'none';
-    redVidDiv.style.zIndex = '0';
-    BeYouTyDiv.style.zIndex = '0';
   },1000);
 })
 
@@ -31,12 +42,24 @@ redVidDiv.addEventListener('click',function(){
   redVidDiv.style.height = '42vw';
   redVidDiv.style.left = '20.5vw';
   redVidDiv.style.zIndex = '2';
-  redVid.src = 'assets/video/redProt.webm'
   blackLayer.style.display = 'block';
+
+  shadowLayer[0].style.display = 'none';
+  shadowLayer[1].style.display = 'none';
+  shadowLayer[2].style.display = 'none';
+
+  blackLayer.style.backgroundColor = 'rgba(218,217,228)';
 
   setTimeout(function(){
     blackLayer.style.opacity = '1';
   },20);
+
+  setTimeout(function(){
+    redVid.style.width = 'auto';
+    redVid.style.height = 'auto'
+    redVid.src = 'assets/video/protoFixd.webm'
+  },900);
+
 
 });
 
@@ -44,11 +67,21 @@ BeYouTyDiv.addEventListener('click',function(){
   BeYouTyDiv.style.width = '20vw';
   BeYouTyDiv.style.height = '42vw';
   BeYouTyDiv.style.zIndex = '2';
-  beyoutyVid.src = 'assets/video/nails.webm';
   blackLayer.style.display = 'block';
+  shadowLayer[0].style.display = 'none';
+  shadowLayer[1].style.display = 'none';
+  shadowLayer[2].style.display = 'none';
+
+  blackLayer.style.backgroundColor = 'white';
 
   setTimeout(function(){
     blackLayer.style.opacity = '1';
   },20);
+
+  setTimeout(function(){
+    beyoutyVid.style.height = 'auto';
+    beyoutyVid.style.width = 'auto';
+    beyoutyVid.src = 'assets/video/nailsOne.webm';
+  },900);
 
 });
