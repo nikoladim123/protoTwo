7ac471bd13834abe28564f5bf8e5c5dcf39fb6e4
unknown <nikoladimitrijevic1991@gmail.com>
2019-03-21 18:44:22 +0100

    prot fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,8 @@ exitButton.addEventListener('click',()=>{
   redVidDiv.style.height = '36.795vw';
   BeYouTyDiv.style.height = '36.795vw';
   redVid.style.width = '100%';
-  redVid.style.height = '100%'
+  redVid.style.height = '100%';
+  redVid.style.left = '0';
   beyoutyVid.style.width = '100%';
   beyoutyVid.style.height = '100%';
   redVidDiv.style.zIndex = '0';
@@ -55,9 +56,10 @@ redVidDiv.addEventListener('click',function(){
   },20);
 
   setTimeout(function(){
-    redVid.style.width = 'auto';
-    redVid.style.height = 'auto'
-    redVid.src = 'assets/video/protoFixd.webm'
+    redVid.src = 'assets/video/protoFixd.webm';
+    redVid.style.left = '-5vw';
+    redVid.style.width = '28.6vw';
+    redVid.style.height = '45.6vw'
   },900);
 
 
@@ -68,6 +70,7 @@ BeYouTyDiv.addEventListener('click',function(){
   BeYouTyDiv.style.height = '42vw';
   BeYouTyDiv.style.zIndex = '2';
   blackLayer.style.display = 'block';
+
   shadowLayer[0].style.display = 'none';
   shadowLayer[1].style.display = 'none';
   shadowLayer[2].style.display = 'none';
@@ -85,3 +88,10 @@ BeYouTyDiv.addEventListener('click',function(){
   },900);
 
 });
+
+
+// position: relative;
+// width: 26vw;
+// height: 43vw;
+// left: 14vw;
+// top: -0.3vw;
